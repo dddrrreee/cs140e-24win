6f2f645b949edc00bad4f576c4b5e75f1e8153f3
engler <ddd.rrr.eee@gmail.com>
2024-02-03 17:51:43 -0800

    minor

---
+++ labs/9-device-int/0-circular-example/0-circular-example.d
@@ -0,0 +1,13 @@
+0-circular-example.o: 0-circular-example.c \
+ /home/engler/class/cs140e-24win//libpi/include/rpi.h \
+ /home/engler/class/cs140e-24win//libpi/libc/demand.h \
+ /home/engler/class/cs140e-24win//libpi/include/gpio.h \
+ /home/engler/class/cs140e-24win//libpi/include/rpi-constants.h \
+ /home/engler/class/cs140e-24win//libpi/include/your-prototypes.h \
+ /home/engler/class/cs140e-24win//libpi/include/timer-interrupt.h \
+ /home/engler/class/cs140e-24win//libpi/include/rpi.h \
+ /home/engler/class/cs140e-24win//libpi/include/rpi-armtimer.h \
+ /home/engler/class/cs140e-24win//libpi/include/rpi-interrupts.h \
+ /home/engler/class/cs140e-24win//libpi/libc/circular.h \
+ /home/engler/class/cs140e-24win//libpi/include/rpi-interrupts.h \
+ /home/engler/class/cs140e-24win//libpi/include/rpi-inline-asm.h
