83b5b05ac0868481fd71f4127159e13bc84d9a17
engler <ddd.rrr.eee@gmail.com>
2024-02-08 18:44:42 -0800

    minor

---
+++ labs/10-debug-hw/code/6-mini-watch-byte-access.c
@@ -0,0 +1,88 @@
+// test of:
+//   1. the debug interface (see debug.c)
+//   2. that you can trap on sub-word read/writes 
+
+#include "rpi.h"
+#include "mini-watch.h"
+
+// set these before each fault so we can check that the right 
+// thing happened.
+static volatile uint32_t expected_fault_addr;
+static volatile uint32_t expected_fault_pc;
+static volatile uint32_t load_fault_n, store_fault_n;
+
+// change to passing in the saved registers.
+static void 
+watchpt_handler(void *data, watch_fault_t *w) {
+    if(w->fault_addr != (void*)expected_fault_addr)
+        panic("expected watchpt fault on addr %p, have %p\n", 
+                        expected_fault_addr, w->fault_addr);
+    if(w->fault_pc != expected_fault_pc)
+        panic("expected watchpt fault at pc=%p, faulted at=%p\n", 
+                        expected_fault_pc, w->fault_pc);
+
+    if(w->is_load_p) {
+        output("load fault at pc=%x, addr=%x\n", w->fault_pc, w->fault_addr);
+        assert(w->fault_pc == (uint32_t)GET8);
+        load_fault_n++;
+    } else {
+        output("store fault at pc=%x, addr=%x\n", w->fault_pc, w->fault_addr);
+        assert(w->fault_pc == (uint32_t)PUT8);
+        store_fault_n++;
+    }
+    mini_watch_disable(w->fault_addr);
+}
+
+//test that watchpoint works:
+//  1. set a watchpoint on <fault_addr>, 
+//  2. do a PUT8(fault_addr), check result.
+//  3. do a GET8(fault_addr), check result.
+static void test_bad_addr(uint32_t fault_addr) {
+    static uint8_t val = 0xfa;
+    uint8_t got = 0;
+
+    // keep changing the value from run to run
+    val = val*val;
+
+    expected_fault_addr = fault_addr;
+    trace("about to test addr %x\n", fault_addr);
+
+    // one-time setup for this address: 
+    //  install the watchpoint handler.
+    mini_watch_addr((void*)fault_addr);
+    assert(mini_watch_enabled());
+    trace("set watchpoint for addr %p\n", fault_addr);
+
+    trace("should see a store fault!\n");
+    expected_fault_pc = (uint32_t)PUT8;
+    PUT8(fault_addr,val);
+    assert(!mini_watch_enabled());
+
+    if(!store_fault_n)
+        panic("did not see a store fault\n");
+
+    got = GET8(fault_addr);
+    if(got != val)
+        panic("stored %x, but got %x\n", val, got);
+
+    // since already watching <fault_addr> just re-enable.
+    mini_watch_addr((void*)fault_addr);
+    assert(mini_watch_enabled());
+
+    trace("should see a load fault!\n");
+    expected_fault_pc = (uint32_t)GET8;
+    got = GET8(fault_addr);
+    if(!load_fault_n)
+        panic("did not see a load fault\n");
+    if(got != val)
+        panic("stored %x, but got %x\n", val, got);
+}
+
+void notmain(void) {
+    mini_watch_init(watchpt_handler, 0);
+
+    for(int i = 0; i < 4; i++)
+        test_bad_addr(i);
+
+    trace("SUCCESS\n");
+}
--- labs/10-debug-hw/code/mini-watch.c
+++ labs/10-debug-hw/code/mini-watch.c
@@ -1,1 +1,84 @@
-void not_implemented_yet(void) {}
+// very dumb, simple interface to wrap up watchpoints better.
+// only handles a single watchpoint.
+//
+// You should be able to take most of the code from the 
+// <1-watchpt-test.c> test case where you already did 
+// all the needed operations.  This interface just packages 
+// them up a bit.
+//
+// possible extensions:
+//   - do as many as hardware supports, perhaps a handler for 
+//     each one.
+//   - make fast.
+//   - support multiple independent watchpoints so you'd return
+//     some kind of structure and pass it in as a parameter to
+//     the routines.
+#include "rpi.h"
+#include "mini-watch.h"
+
+// we have a single handler: so just use globals.
+static watch_handler_t watchpt_handler = 0;
+static void *watchpt_data = 0;
+
+// is it a load fault?
+static int mini_watch_load_fault(void) {
+    todo("implement");
+}
+
+// if we have a dataabort fault, call the watchpoint
+// handler.
+static void watchpt_fault(regs_t *r) {
+    // watchpt handler.
+    if(was_brkpt_fault())
+        panic("should only get debug faults!\n");
+    if(!was_watchpt_fault())
+        panic("should only get watchpoint faults!\n");
+    if(!watchpt_handler)
+        panic("watchpoint fault without a fault handler\n");
+
+    watch_fault_t w = {0};
+
+    todo("setup the <watch_fault_t> structure");
+    todo("call: watchpt_handler(watchpt_data, &w);");
+
+    // in case they change the regs.
+    switchto(w.regs);
+}
+
+// setup:
+//   - exception handlers, 
+//   - cp14, 
+//   - setup the watchpoint handler
+// (see: <1-watchpt-test.c>
+void mini_watch_init(watch_handler_t h, void *data) {
+    todo("setup cp14 and the full exception routines");
+
+    // just started, should not be enabled.
+    assert(!cp14_bcr0_is_enabled());
+    assert(!cp14_bcr0_is_enabled());
+
+    watchpt_handler = h;
+    watchpt_data = data;
+}
+
+// set a watch-point on <addr>.
+void mini_watch_addr(void *addr) {
+    todo("watch <addr>");
+    assert(cp14_wcr0_is_enabled());
+}
+
+// disable current watchpoint <addr>
+void mini_watch_disable(void *addr) {
+    todo("implement");
+}
+
+// return 1 if enabled.
+int mini_watch_enabled(void) {
+    todo("implement");
+}
+
+// called from exception handler: if the current 
+// fault is a watchpoint, return 1
+int mini_watch_is_fault(void) { 
+    todo("implement");
+}
